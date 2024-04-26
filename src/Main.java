afe5659982b131736f8d674b7055563670831c6e
Farel <falfarizihasan@gmail.com>
2024-04-26 16:31:15 +0200

    added a new print line my father is my hero

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Kartik is a hero");
+        System.out.println("my Father is my hero");
     }
 }
