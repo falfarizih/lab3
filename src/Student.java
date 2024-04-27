0bafed29d4cac5d8e1e2f1c87f12e0163bb954ec
kartik <kartik.kushwaha.3011@gmail.com>
2024-04-27 20:01:25 +0200

    I added a new class called Student

---
+++ src/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+    private int matrikelNummer;
+    private String name;
+
+    public Student(int matrikelNummer, String name) {
+
+        this.matrikelNummer = matrikelNummer;
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMatrikelNummer() {
+        return matrikelNummer;
+    }
+}
